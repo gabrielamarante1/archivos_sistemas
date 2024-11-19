dc477f70f7e49d7436f06ff0f233800ede625adf
gabrielamarante1 <153243893+gabrielamarante1@users.noreply.github.com>
2024-11-19 00:15:10 -0300

    Update backup_prestamos.sh

--- backup_prestamos.sh
+++ backup_prestamos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 FECHA=$(date +%F)
 NOMBRE_BACKUP="prestamossoft_backup_$FECHA.sql"
-mysqldump -u prestausuario -p prestacontraseña PrestamosElPinar > /$HOME/Documentos/PrestamosSoft/$NOMBRE_BACKUP
+mysqldump -u prestausuario -pprestacontraseña PrestamosElPinar > /$HOME/Documentos/PrestamosSoft/$NOMBRE_BACKUP
 echo "Backup creado en /$HOME/Documentos/PrestamosSoft/$NOMBRE_BACKUP"
 
