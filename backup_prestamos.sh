662c12980be9a68dd71909b02d59fba901138958
gabrielamarante1 <153243893+gabrielamarante1@users.noreply.github.com>
2024-12-09 15:35:42 -0300

    Add files via upload

---
+++ backup_prestamos.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Definimos la fecha y el nombre del archivo de backup
+FECHA=$(date +%F)
+NOMBRE_BACKUP="prestamossoft_backup_$FECHA.sql"
+
+# Ruta completa del directorio donde se guardará el backup
+DIRECTORIO_BACKUP="./Backup"
+
+# Crear la carpeta Backup si no existe
+mkdir -p "$DIRECTORIO_BACKUP"
+
+# Ejecutamos el backup
+mysqldump -u prestausuario -pprestacontraseña PrestamosElPinar > "$DIRECTORIO_BACKUP/$NOMBRE_BACKUP"
+
+# Mensaje de confirmación
+echo "Backup creado en $DIRECTORIO_BACKUP/$NOMBRE_BACKUP"
+
