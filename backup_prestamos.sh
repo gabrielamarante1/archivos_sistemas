be83133f7574b531f74cd0d0faaed602c47613d4
gabrielamarante1 <153243893+gabrielamarante1@users.noreply.github.com>
2024-12-09 15:35:20 -0300

    Delete backup_prestamos.sh

--- backup_prestamos.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-FECHA=$(date +%F)
-NOMBRE_BACKUP="prestamossoft_backup_$FECHA.sql"
-mysqldump -u prestausuario -pprestacontraseña PrestamosElPinar > /$HOME/Documentos/PrestamosSoft/$NOMBRE_BACKUP
-echo "Backup creado en /$HOME/Documentos/PrestamosSoft/$NOMBRE_BACKUP"
-
